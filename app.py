0cd1df13e3e161417f20bc87fac837f92ebdfb18
paulofilter <paulofilterbr@gmail.com>
2025-05-25 18:00:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    with open('refined_signals.json', 'r', encoding='utf-8') as f:
+        signals = json.load(f)
+    return render_template('index.html', signals=signals)
